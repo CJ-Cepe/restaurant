c3d2bd7c00abe8cea6b4104951a8bdbfd93e849e
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-22 18:02:31 +0800

    feat: create new js file with function that appends html elements

---
+++ src/load-page.js
@@ -0,0 +1,22 @@
+function loadPage() {
+    const body = document.querySelector('body');
+    const content = document.querySelector('#content');
+    const navigator = document.createElement('nav');
+    const title = document.createElement('span')
+    const option = document.createElement('div')
+    const home = document.createElement('span')
+    const menu = document.createElement('span')
+    const location = document.createElement('span')
+
+    title.textContent = "Chick'n Dip"
+    home.textContent = "Home"
+    menu.textContent = "Menu"
+    location.textContent = "Location"
+
+    body.appendChild(navigator)
+    navigator.append(title, option)
+    option.append(home, menu, location)
+}
+
+
+export {loadPage}
