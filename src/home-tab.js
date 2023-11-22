4b0385ef350038d1c7bffa1f2b19d545822c10e1
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-22 23:00:03 +0800

    feat: add home page elements

---
+++ src/home-tab.js
@@ -0,0 +1,44 @@
+import chicken from './assets/chick-home-0.png'
+
+function loadHome(content){
+    
+    const homeContent = document.createElement('div')
+    homeContent.classList.add('home', 'tab-page')
+    const left = document.createElement('div')
+    const right = document.createElement('div')
+    const image = document.createElement('img')
+    const header = document.createElement('span')
+    const flavor = document.createElement('div')
+    const aboutUs = document.createElement('div')
+    const about1 = document.createElement('p')
+    const about2 = document.createElement('p')
+    const contactUs = document.createElement('div')
+    const number = document.createElement('span')
+    const email = document.createElement('span')
+
+    //clear Content
+    content.textContent = '';
+    content.appendChild(homeContent)
+    homeContent.append(left, right);
+    left.append(header, aboutUs, contactUs);
+    right.appendChild(image);
+    
+    header.textContent = "Where Chicken Meets"
+    flavor.textContent = 'flavor'
+    header.appendChild(flavor)
+
+    aboutUs.append(about1, about2)
+    about1.textContent = "We're a fried chicken restaurant that celebrates the global love for this delicious dish. Our menu features crispy, juicy, and flavorful chicken recipes, each paired with a homemade dip. "
+    about2.textContent = "At Chick'n Dip, we're not just serving food - we're serving experiences. So come on in, and get ready to dip into deliciousness!"
+    
+    contactUs.textContent = "Contact Us:"
+    contactUs.append(number, email)
+    number.textContent = '+1 (123) 456-7890'
+    email.textContent = 'info@chickndip.com'
+
+    image.src = chicken
+    image.style.width = '70%';
+    image.style.height = 'auto';
+}
+
+export {loadHome}
