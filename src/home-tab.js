c8407bcfbb1508656007c3da2cc9ad6779711e06
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-23 18:32:42 +0800

    refactor: change html structure and styling rules implementing grid display & scroll snapping

--- src/home-tab.js
+++ src/home-tab.js
@@ -2,12 +2,9 @@ import chicken from './assets/chick-home-0.png'
 
 function loadHome(content){
     
-    const homeContent = document.createElement('div')
-    homeContent.classList.add('home', 'tab-page')
-    const left = document.createElement('div')
-    const right = document.createElement('div')
+    const home = document.createElement('div')
+    home.classList.add('home', 'tab-page')
     const image = document.createElement('img')
-    const headerCont = document.createElement('div')
     const header = document.createElement('span')
     const flavor = document.createElement('div')
     const aboutUs = document.createElement('div')
@@ -19,24 +16,19 @@ function loadHome(content){
 
     //clear Content
     content.textContent = '';
-    content.appendChild(homeContent)
-    homeContent.append(left, right);
-    left.append(headerCont, aboutUs, contactUs);
-    left.classList.add('left')
-    right.appendChild(image);
-    right.classList.add('right')
+    content.appendChild(home)
+    home.append(aboutUs, contactUs, image);
     
-    headerCont.appendChild(header)
+    header.appendChild(flavor)
     header.textContent = "Where Chicken Meets"
     flavor.textContent = 'flavor'
-    header.appendChild(flavor)
-
+    
     aboutUs.append(about1, about2)
     about1.textContent = "We're a fried chicken restaurant that celebrates the global love for this delicious dish. Our menu features crispy, juicy, and flavorful chicken recipes, each paired with a homemade dip. "
     about2.textContent = "At Chick'n Dip, we're not just serving food - we're serving experiences. So come on in, and get ready to dip into deliciousness!"
     
-    contactUs.textContent = "Contact Us:"
     contactUs.append(number, email)
+    contactUs.textContent = "Contact Us:"
     number.textContent = '+1 (123) 456-7890'
     email.textContent = 'info@chickndip.com'
 
